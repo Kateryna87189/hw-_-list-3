a0c4426ece07f80ba5006ab4e7df9663ee96d143
Kateryna Nuzhna <katranchik21@gmail.com>
2024-10-23 16:27:58 +0200

    finaly code

--- src/Main.java
+++ src/Main.java
@@ -9,21 +9,59 @@
 Как только пользователь заканчивает ввод (например , вместо номера товара вводит 0)
 программа выдает список покупок.
 */
-
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 import static java.util.List.*;
 
 public class Main {
     public static void main(String[] args) {
-
-        List<Store> products = of(
-                new Store("Laptop", 1, 1000),
-                new Store("Smartphone", 2, 500),
-                new Store("Headphones", 3, 100),
-                new Store("Monitor", 4, 200),
-                new Store("Mouse", 5, 25)
+        List<Products> products = of(
+                new Products(1, "Laptop", 1000.0),
+                new Products(2, "Smartphone", 500.0),
+                new Products(3, "Headphones", 100.0),
+                new Products(4, "Monitor", 200.0),
+                new Products(5, "Mouse", 25.0)
         );
 
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Object> cart = new ArrayList<>();
+
+        System.out.println("Доступні продукти:");
+        for (Products product: products) {
+            System.out.println(products);
+        }
+
+        while (true) {
+            System.out.print("Натисни номер продукту в картці (або 0 для завершення): ");
+            int productId = scanner.nextInt();
+
+            if (productId == 0) {
+                break;
+            }
+
+            Products selectedProduct = null;
+            for (Products product : products) {
+                if (product.getId() == productId) {
+                    selectedProduct = product;
+                    break;
+                }
+            }
+
+            if (selectedProduct != null) {
+                boolean add = cart.add(selectedProduct);
+
+                    System.out.println("Введення в кошику: " + selectedProduct);
+
+            } else {
+                System.out.println("товар не знайдений.");
+            }
+        }
+
+        System.out.println("Ваш кошик:");
+        for (Object product : cart) {
+            System.out.println(product);
+        }
     }
 }
---
+++ src/Products.java
@@ -0,0 +1,28 @@
+public class Products {
+    private int id;
+    private String name;
+    private double price;
+
+    public Products(int id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Товар {id=%d, назва='%s', ціна=%.2f Euro%n}", id, name, price);
+    }
+}
